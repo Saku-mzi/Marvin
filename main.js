5b2cca201a8cdea5e1e669fb99a8b7e7a87e7b58
Saku-mzi <sakumzisipamla@gmail.com>
2026-08-20 04:48:30 +0200

    Refactor code structure for improved readability and maintainability

--- main.js
+++ main.js
@@ -1,55 +1,25 @@
 import data from "./data.json" with { type: "json" };
-console.log(data);
-const qNum = ["#66", "#72", "#43", "#92", "#77", "#33", "#42", "#93", "#11"];
-const quote = [
-  `"`,
-  `"`,
-  `"`,
-  `"`,
-  `"`,
-  `"`,
-  `Hee Hee"`,
-  `"`,
-  `We are for the GOAT"`,
-];
-
-const images = [
-  `images/20.jpg`,
-  `images/05.jpg`,
-  `images/06.jpg`,
-  `images/10.jpg`,
-  /*
-  `images/11.jpg`,
-  `images/13.jpg`,
-  `images/21.jpg`,
-  `images/24.jpg`,
-  `images/cantona.jpg`,
-  */
-];
-
-const name = [``, ``, ``, ``, ``, ``, `Michael Jackson`, ``, ``];
 
 let pixels = 0;
 const photo = document.getElementById("photo");
 const quoteNumber = document.getElementById("quoteNumber");
 const quotes = document.getElementById("quoteOfTheDay");
 const names = document.getElementById("naam");
+const cRight = document.getElementById("copyright");
 
 function show(item) {
   photo.src = item.src;
   quoteNumber.textContent = `#${item.number}`;
-  photo.alt = images[pixels];
-  quotes.textContent = quote[pixels];
-  names.textContent = name[pixels];
-  console.log(pixels, item);
+  quotes.textContent = item.quote;
+  names.textContent = item.author;
+  cRight.textContent = item.copyright;
 }
 
 document.getElementById("marvinImage").onclick = () => {
-  if (pixels == data.gallery.length - 1) {
+  if (pixels === data.gallery.length - 1) {
     pixels = 0;
   } else {
     pixels++;
   }
-  //console.log(gallery, pixels, gallery[pixels]);
   show(data.gallery[pixels]);
 };
